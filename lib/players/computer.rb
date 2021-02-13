2da3e6c383320620eec82152e9c727c9fe8ebe95
Ben Altman <altmanguitar@gmail.com>
2021-02-13 00:56:04 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -19,7 +19,7 @@ binding.pry
 #binding.pry
           if combo.select{|e| board.cells[e]==self.token}.size==2 && combo.any? {|e| board.cells[e]==" "}
             input = combo.select{|e| !board.taken?(e)}.first.to_i.+(1).to_s
-#           input = combo.detect{|e| board.cells[e]==" "}.to_s
+#           input = combo.detect{|e| board.cells[e]==" "}.to_i.+1.to_s
           elsif combo.select{|e| board.cells[e]!=self.token &&
             board.cells[e]!=" "}.count == 2
             input = combo.detect{|e| board.cells[e]==" "}.+1.to_s
